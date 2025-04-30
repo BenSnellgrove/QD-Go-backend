e79496ed9d3f303a834e5662301661e8c3c6b082
BenSnellgrove <ben.e.snellgrove@gmail.com>
2025-04-30 14:15:10 +0100

    accounts not account

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ func main() {
 
 	v1 := r.Group("/api/v1")
 	{
-		v1.GET("/account/:id", getAccount)
+		v1.GET("/accounts/:id", getAccount)
+		v1.GET("/insights/:id", getAccount)
 	}
 
 	// Swagger docs route
@@ -27,11 +28,37 @@ type Account struct {
 	ID   int    `json:"id" example:"1"`
 	Name string `json:"name" example:"Current account"`
 	Balance float32 `json:"balance" example:"100.0"`
-	SortCode string `json:"sort_code" example:"12-34-56"`
-	AccountNumber string `json:"account_number" example:"12345687"`
+	SortCode string `json:"sortCode" example:"12-34-56"`
+	AccountNumber string `json:"accountNumber" example:"12345687"`
 }
 
 func getAccount(c *gin.Context) {
+	id := c.Param("id")
+
+	switch id {
+		case "1":
+			c.JSON(http.StatusOK, Account{ID: 1, Name: "Personal Current Account", Balance: 2023.00, SortCode: "12-45-78", AccountNumber: "12345678"})
+		case "2":
+			c.JSON(http.StatusOK, Account{ID: 2, Name: "Family Savings", Balance: 10000.00, SortCode: "12-45-78", AccountNumber: "87654321"})
+		case "3":
+			c.JSON(http.StatusOK, Account{ID: 3, Name: "Mortgage Account", Balance: 155685.00, SortCode: "", AccountNumber: "987650234"})
+		case "4":
+			c.JSON(http.StatusOK, Account{ID: 4, Name: "Sam's Children's Account", Balance: 100.00, SortCode: "12-45-78", AccountNumber: "98766443"})
+		case "5":
+			c.JSON(http.StatusOK, Account{ID: 5, Name: "Jess' Children's Account", Balance: 130.00, SortCode: "12-45-78", AccountNumber: "12345668"})
+		case "6":
+			c.JSON(http.StatusOK, Account{ID: 6, Name: "Max's Children's Account", Balance: 600.00, SortCode: "12-45-78", AccountNumber: "12344678"})
+		case "7":
+			c.JSON(http.StatusOK, Account{ID: 7, Name: "Children's Current Account (A Jones)", Balance: 500.00, SortCode: "12-45-78", AccountNumber: "85274169"})
+		case "8":
+			c.JSON(http.StatusOK, Account{ID: 8, Name: "Junior ISA (A Jones)", Balance: 2023.00, SortCode: "12-45-78", AccountNumber: "45679888"})
+		default:
+			c.JSON(http.StatusBadRequest, nil)
+
+	}
+}
+
+func getInsight(c *gin.Context) {
 	id := c.Param("id")
 
 	switch id {
